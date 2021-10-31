89feeb30e181ae70e51f56137d05e0c048316eb9
孙雨辰 <syc12466@163.com>
2021-10-31 18:30:58 +0800

    你好GitHub

---
+++ HellowGitHub.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: sz
+ * @Description: TODO
+ * @DateTime: 2021/10/31$ 18:30$
+ */
+public class HellowGitHub {
+    public static void main(String[] args) {
+        System.out.println("你好GitHub");
+    }
+}
